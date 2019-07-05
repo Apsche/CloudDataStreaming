80f013b7b90bded84c0dd542e55149d933f347c0
Ryan Apsche <Apsche@users.noreply.github.com>
2019-07-05 09:54:48 -0400

    Updated Properties

--- streamer.java
+++ streamer.java
@@ -21,9 +21,14 @@ public Class streamer
 		// The configuration values defined in StreamsConfig must be mapped
 		Properties props = new Properties();
 		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streamer");
-		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-		props.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-		props.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "pkc-ldrz4.us-east-2.aws.confluent.cloud:9092");
+		props.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaAvroSerializer");
+		props.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaAvroSerializer");
+		props.put(StreamsConfig.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
+		props.put(SASL_MECHANISM, "PLAIN");
+		props.put(SASL_JAAS_CONFIG, "org.apache.kafka.common.security.plain.PlainLoginModule \ username=7BYZ6FIYJIJQ7NCR \ " +
+			"password= \ Du+0pUmUl5rabqiJQGO20EBlpTHhk7AnEpC2V02WuljKhw0hQBdnE7uKUzGK5zkY \ ;");//Not sure if i did this one right
+		props.put(StreamsConfig.producerPrefix(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG), 20000);
 
 		// Defines computational logic of streams builder
 		final StreamsBuilder builder = new StreamsBuilder();

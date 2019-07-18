f48ea6c0562ae233b99c515c23c23e2b98670630
Jacob Weidokal <jweidokal@yahoo.com>
2019-07-18 17:22:44 -0400

    trying to implement websockets

--- ConsoleConsumer/ConsoleConsumer/Program.cs
+++ ConsoleConsumer/ConsoleConsumer/Program.cs
@@ -8,11 +8,129 @@ using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
 
 namespace ConsoleConsumer
 {
     class Program
     {
+        static Socket _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+        static private string _guid = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+
+        public static void Main(string[] args)
+        {
+            var configPath = args[0];
+            var certDir = args[1];
+
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Any, 8080));
+            _serverSocket.Listen(128);
+            _serverSocket.BeginAccept(null, 0, OnAccept, null);
+
+            // try
+            // {
+            //     var config = LoadConfig(configPath, certDir).Result;
+            //     Consume("Weather", config);
+            // }
+
+            // catch (Exception e)
+            // {
+            //     Console.WriteLine(e.Message);
+            // }
+
+            Console.ReadLine();   
+        }
+
+        private static void OnAccept(IAsyncResult result) 
+        {
+            byte[] buffer = new byte[1024];
+            try
+            {
+                Socket client = null;
+                string headerResponse = "";
+                if (_serverSocket != null && _serverSocket.IsBound)
+                {
+                    client = _serverSocket.EndAccept(result);
+                    var i = client.Receive(buffer);
+                    headerResponse = (System.Text.Encoding.UTF8.GetString(buffer)).Substring(0,i);
+                    // write received data to the console
+                    Console.WriteLine(headerResponse);
+
+                }
+                if (client != null)
+                {
+                    /* Handshaking and managing ClientSocket */
+                    var key = headerResponse.Replace("ey:", "`")
+                              .Split('`')[1]                     // dGhlIHNhbXBsZSBub25jZQ== \r\n .......
+                              .Replace("\r", "").Split('\n')[0]  // dGhlIHNhbXBsZSBub25jZQ==
+                              .Trim();
+
+                    // key should now equal dGhlIHNhbXBsZSBub25jZQ==
+                    var test1 = AcceptKey(ref key);
+
+                    var newLine = "\r\n";
+
+                    var response = "HTTP/1.1 101 Switching Protocols" + newLine
+                         + "Upgrade: websocket" + newLine
+                         + "Connection: Upgrade" + newLine
+                         + "Sec-WebSocket-Accept: " + test1 + newLine + newLine
+                         //+ "Sec-WebSocket-Protocol: chat, superchat" + newLine
+                         //+ "Sec-WebSocket-Version: 13" + newLine
+                         ;
+
+                    // which one should I use? none of them fires the onopen method
+                    client.Send(System.Text.Encoding.UTF8.GetBytes(response));
+
+                    var i = client.Receive(buffer); // wait for client to send a message
+
+                    // once the message is received decode it in different formats
+                    Console.WriteLine(Convert.ToBase64String(buffer).Substring(0, i));                    
+
+                    Console.WriteLine("\n\nPress enter to send data to client");
+                    Console.Read();
+
+                    var subA = SubArray<byte>(buffer, 0, i);
+                    client.Send(subA);
+                    Thread.Sleep(10000); //wait for message to be send
+                    
+
+                }
+            }
+            catch (SocketException exception)
+            {
+                throw exception;
+            }
+            finally
+            {
+                if (_serverSocket != null && _serverSocket.IsBound)
+                {
+                    _serverSocket.BeginAccept(null, 0, OnAccept, null);
+                }
+            }
+        }
+
+        public static T[] SubArray<T>(T[] data, int index, int length)
+        {
+            T[] result = new T[length];
+            Array.Copy(data, index, result, 0, length);
+            return result;
+        }
+
+        private static string AcceptKey(ref string key)
+        {
+            string longKey = key + _guid;
+            byte[] hashBytes = ComputeHash(longKey);
+            return Convert.ToBase64String(hashBytes);
+        }
+
+        static SHA1 sha1 = SHA1CryptoServiceProvider.Create();
+        private static byte[] ComputeHash(string str)
+        {
+            return sha1.ComputeHash(System.Text.Encoding.ASCII.GetBytes(str));
+        }
+
+
         public static async Task<ClientConfig> LoadConfig(string configPath, string certDir)
         {
             try
@@ -87,28 +205,6 @@ namespace ConsoleConsumer
                     consumer.Close();
                 }
             }
-        }
-
-
-        static async Task Main(string[] args)
-        {
-            var configPath = args[0];
-            var certDir = args[1];
-
-            try
-            {
-                var config = await LoadConfig(configPath, certDir);
-                Consume("Weather", config);
-                Console.WriteLine("Success");
-            }
-
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-            Console.ReadLine();
-            
         }
     }
 }
